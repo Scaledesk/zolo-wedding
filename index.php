099889177f87007494b10a3872ee28d08d77c596
Neeraj <nkneeraj99@gmail.com>
2016-07-20 15:16:22 +0530

    added new Analytics code

--- index.php
+++ index.php
@@ -1713,5 +1713,25 @@
 
 </script>
 
+
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 878949233;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "LuSaCMq_yGgQ8eaOowM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/878949233/?label=LuSaCMq_yGgQ8eaOowM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+
 </body>
 </html>
--- thankyou.php
+++ thankyou.php
@@ -154,6 +154,35 @@
 <!--<script src="js/gmap.js"></script>-->
 <script src="js/retina.min.js"></script>
 <!--<script src="bower_components/bootstrap-multiselect/dist/js/bootstrap-multiselect.js"></script>-->
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID --> <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-80602802-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 878949233;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "LuSaCMq_yGgQ8eaOowM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/878949233/?label=LuSaCMq_yGgQ8eaOowM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
     
 
 </body>
