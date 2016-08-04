94dab66bac8a32cd451e9df43453a13a3b971b26
Neeraj <nkneeraj99@gmail.com>
2016-08-04 16:38:14 +0530

    added new google script to zolo thank you page

--- thankyou.php
+++ thankyou.php
@@ -166,13 +166,14 @@
 </script>
 
 
+<!-- Google Code for Wedding Leads Conversion Page -->
 <script type="text/javascript">
 /* <![CDATA[ */
 var google_conversion_id = 878949233;
 var google_conversion_language = "en";
 var google_conversion_format = "3";
 var google_conversion_color = "ffffff";
-var google_conversion_label = "LuSaCMq_yGgQ8eaOowM";
+var google_conversion_label = "w0bUCIPv92gQ8eaOowM";
 var google_remarketing_only = false;
 /* ]]> */
 </script>
@@ -180,9 +181,11 @@ var google_remarketing_only = false;
 </script>
 <noscript>
 <div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/878949233/?label=LuSaCMq_yGgQ8eaOowM&amp;guid=ON&amp;script=0"/>
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/878949233/?label=w0bUCIPv92gQ8eaOowM&amp;guid=ON&amp;script=0"/>
 </div>
 </noscript>
+
+
     
 
 </body>
